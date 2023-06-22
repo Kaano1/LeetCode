92a6b6e035d64d3c4be99238cad300d1e4994742
Kaano1 <ahmetyumusakdiken13@gmail.com>
2023-06-22 18:25:42 +0300

    1491. Average Salary Excluding the Minimum and Maximum Salary solved

---
+++ Easy/1491. Average Salary Excluding the Minimum and Maximum Salary/average.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        double cal;
+    
+        cal = accumulate(salary.begin(),salary.end(),0);
+        cal -= *max_element(salary.begin(), salary.end());
+        cal -=  *min_element(salary.begin(), salary.end());
+        cal /= (salary.size() - 2);
+        return (cal);
+    }
+};
